63caefe644ce93d195db6825cc57bbad16a4a18e
Andrew Lau <15017794+Gadget-Geek@users.noreply.github.com>
2018-04-21 15:18:45 -0400

    Update HomeController.cs
    See Listing 4-28 in book

--- Source Code 1-31/04 - Language Features/LanguageFeatures/src/LanguageFeatures/Controllers/HomeController.cs
+++ Source Code 1-31/04 - Language Features/LanguageFeatures/src/LanguageFeatures/Controllers/HomeController.cs
@@ -2,22 +2,36 @@
 using System.Collections.Generic;
 using LanguageFeatures.Models;
 using System;
+using System.Text;
 using System.Linq;
 
 namespace LanguageFeatures.Controllers {
     public class HomeController : Controller {
 
-        public ViewResult Index() {
+        public ViewResult FindProducts() {
 
             var products = new[] {
-                new { Name = "Kayak", Price = 275M },
-                new { Name = "Lifejacket", Price = 48.95M },
-                new { Name = "Soccer ball", Price = 19.50M },
-                new { Name = "Corner flag", Price = 34.95M }
+                new { Name = "Kayak", Category = "Watersports", Price = 275M },
+                new { Name = "Lifejacket", Category = "Watersports", Price = 48.95M },
+                new { Name = "Soccer ball", Category = "Soccer", Price = 19.50M },
+                new { Name = "Corner flag", Category = "Soccer", Price = 34.95M }
             };
 
-            return View(products.Select(p =>
-                $"{nameof(p.Name)}: {p.Name}, {nameof(p.Price)}: {p.Price}"));
+            var foundProducts = from match in products
+                                orderby match.Price descending
+                                select new { match.Name, match.Price };
+            
+            // create the result
+            int count = 0;
+            StringBuilder result = new StringBuilder();
+            foreach (var p in foundProducts) {
+                result.AppendFormat("Price: {0} ", p.Price);
+                if (++count == 3) {
+                    break;
+                }
+            }
+            
+            return View("Result", (object)result.ToString());
         }
     }
 }
